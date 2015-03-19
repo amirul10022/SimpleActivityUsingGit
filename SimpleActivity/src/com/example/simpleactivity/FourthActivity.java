1846e89697ada7fd507fc11d0f846f2b7fbafcfc
amirul <bhuiyan.amirul@yahoo.com>
2015-03-19 21:54:39 +0600

    Add Fifth Activity

--- SimpleActivity/src/com/example/simpleactivity/FourthActivity.java
+++ SimpleActivity/src/com/example/simpleactivity/FourthActivity.java
@@ -1,9 +1,10 @@
 package com.example.simpleactivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.view.View;
 
 public class FourthActivity extends Activity {
 
@@ -11,5 +12,13 @@ public class FourthActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fourth);
+		
+	}
+	
+	public void onClick(View view){
+    	
+    	Intent intent3= new Intent(this, FifthActivity.class);
+    	intent3.putExtra("URL", "http://www.vogella.com");
+    	startActivity(intent3);
 	}
 }
