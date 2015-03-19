e5ff55781b89226d6d0e55c27041bcb75950da82
amirul <bhuiyan.amirul@yahoo.com>
2015-03-19 21:34:03 +0600

    Add an Activity

--- SimpleActivity/src/com/example/simpleactivity/ThirdActivity.java
+++ SimpleActivity/src/com/example/simpleactivity/ThirdActivity.java
@@ -1,9 +1,11 @@
 package com.example.simpleactivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 /*import android.view.Menu;
 import android.view.MenuItem;*/
+import android.view.View;
 
 public class ThirdActivity extends Activity {
 
@@ -12,4 +14,12 @@ public class ThirdActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_third);
 	}
+	
+	 public void onClick(View view){
+	    	
+	    	Intent intent2= new Intent(this, FourthActivity.class);
+	    	intent2.putExtra("URL", "http://www.vogella.com");
+	    	startActivity(intent2);
+	    	
+	    }
 }
